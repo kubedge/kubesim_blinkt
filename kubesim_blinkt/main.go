f341d06a546f3e82d144f857dfb0a9b9b3ad2803
Jerome Brette <jbrette@gmail.com>
2018-10-30 04:35:05 +0000

    Add config file access to control blinking
    Change-Id: I81f4cf04d03f6c28558935860242f58e7af6af62

---
+++ kubesim_blinkt/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"fmt"
+	"github.com/kubedge/kubesim_blinkt/config"
+	"github.com/kubedge/kubesim_blinkt/periBlink"
+	"math/rand"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func delay(ms int) {
+	time.Sleep(time.Duration(ms) * time.Millisecond)
+}
+
+func blinkt5(running bool, conf config.BlinktConfigData) {
+	for running {
+		pixel := rand.Intn(8)
+		periBlink.SetPixel(pixel, rand.Intn(255), rand.Intn(255), rand.Intn(255), rand.Intn(3))
+		periBlink.Show()
+		delay(60)
+	}
+}
+
+func fixed5(running bool, conf config.BlinktConfigData) {
+	for running {
+		if len(conf.Pixel0) != 0 {
+			periBlink.SetPixel(0, conf.Pixel0[0], conf.Pixel0[1], conf.Pixel0[2], conf.Intensity)
+		}
+
+		if len(conf.Pixel1) != 0 {
+			periBlink.SetPixel(0, conf.Pixel1[0], conf.Pixel1[1], conf.Pixel1[2], conf.Intensity)
+		}
+		if len(conf.Pixel2) != 0 {
+			periBlink.SetPixel(0, conf.Pixel2[0], conf.Pixel2[1], conf.Pixel2[2], conf.Intensity)
+		}
+		if len(conf.Pixel3) != 0 {
+			periBlink.SetPixel(0, conf.Pixel3[0], conf.Pixel3[1], conf.Pixel3[2], conf.Intensity)
+		}
+		if len(conf.Pixel4) != 0 {
+			periBlink.SetPixel(0, conf.Pixel4[0], conf.Pixel4[1], conf.Pixel4[2], conf.Intensity)
+		}
+		if len(conf.Pixel5) != 0 {
+			periBlink.SetPixel(0, conf.Pixel5[0], conf.Pixel5[1], conf.Pixel5[2], conf.Intensity)
+		}
+		if len(conf.Pixel6) != 0 {
+			periBlink.SetPixel(0, conf.Pixel6[0], conf.Pixel6[1], conf.Pixel6[2], conf.Intensity)
+		}
+		if len(conf.Pixel7) != 0 {
+			periBlink.SetPixel(0, conf.Pixel7[0], conf.Pixel7[1], conf.Pixel7[2], conf.Intensity)
+		}
+
+		periBlink.Show()
+		delay(conf.Frequency)
+	}
+}
+
+func main() {
+	running := true
+	// initialise getout
+	signalChannel := make(chan os.Signal, 2)
+	signal.Notify(signalChannel, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		sig := <-signalChannel
+		switch sig {
+		case os.Interrupt:
+			fmt.Println("Stopping on Interrupt")
+			running = false
+			return
+		case syscall.SIGTERM:
+			fmt.Println("Stopping on Terminate")
+			running = false
+			return
+		}
+	}()
+
+	periBlink.Setup()
+	periBlink.SetLuminance(1)
+	periBlink.Clear()
+	periBlink.Show()
+
+	var conf config.BlinktConfigData
+	conf.Config()
+
+	if conf.Algorithm == "blinkt5" {
+		blinkt5(running, conf)
+	} else {
+		fixed5(running, conf)
+	}
+	fmt.Println("Stopping")
+	periBlink.Exit()
+}
